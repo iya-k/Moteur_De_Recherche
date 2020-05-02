38298c71ed9adf7cdc2aacae9902e0ae6eb3346b
KABA Saran <skaba1606@gmail.com>
2020-05-03 00:12:16 +0200

    Matrice CLI

--- src/tp1/CLI.java
+++ src/tp1/CLI.java
@@ -1,132 +1,48 @@
 package tp1;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class CLI 
 {
 	private int n;
-    HashMap<Integer, HashMap<Integer, Double>> mots_pages;
-    HashMap<String, Integer> numero_pages;
-    private String lastTitle;
-	private ArrayList<Float> mC;
-	private ArrayList<Integer> mL;
-	private ArrayList<Integer> mI;
+	private LinkedList<Float> mC;
+	private LinkedList<Integer> mL;
+	private LinkedList<Integer> mI;
 
 
 
-	public CLI () 
+	public CLI (Matrice m) 
 	{
-		mC = new ArrayList<Float>();
-		mL = new ArrayList<Integer>();
-		mI = new ArrayList<Integer>();
-	}
-
-	public CLI (ArrayList<Float> c, ArrayList<Integer> l, ArrayList<Integer> i) 
-	{
-		mC = c;
-		mL = l;
-		mI = i;
-	}
-	
-	public  CLI(int size)
-	{
-		n = size;
+		n = m.getMatrice().length;
+		mC = new LinkedList<Float>();
+		mL = new LinkedList<Integer>();
+		mI = new LinkedList<Integer>();
 		
-		mC = new ArrayList<Float>();
-		mL = new ArrayList<Integer>();
-		mI = new ArrayList<Integer>();
-		for (int i = 0; i <= this.n; i++) {
-            this.mL.add(0);
-        }
-	}
-
-	/**
-	 * @return the mC
-	 */
-	public ArrayList<Float> getmC() {
-		return mC;
-	}
-
-	/**
-	 * @param mC the mC to set
-	 */
-	public void setmC(ArrayList<Float> mC) 
-	{
-		this.mC = mC;
-	}
-
-	/**
-	 * @return the mL
-	 */
-	public ArrayList<Integer> getmL() {
-		return mL;
-	}
-
-	/**
-	 * @param mL the mL to set
-	 */
-	public void setmL(ArrayList<Integer> mL) {
-		this.mL = mL;
-	}
-
-	/**
-	 * @return the mI
-	 */
-	public ArrayList<Integer> getmI() {
-		return mI;
-	}
-
-	/**
-	 * @param mI the mI to set
-	 */
-	public void setmI(ArrayList<Integer> mI) {
-		this.mI = mI;
-	}
-
-
-	public void setC(int n,float Mat[][]) 
-	{ 
 		int i,j;
 		for(i=0;i<n;i++) 
 		{ 
 			for(j=0;j<n;j++) 
 			{
-				if(Mat[i][j]!=0) 
+				if(m.getMatrice()[i][j]!=0) 
 				{
-					mC.add(Mat[i][j]); 
+					//System.out.println(m.getMatrice()[i][j]);
+					mC.add(m.getMatrice()[i][j]); 
+					mI.add(j);
 				}
 			}
 		}
-	}
-
-
-	/**
-     * Incrémente les valeurs de L d'index supérieurs ou égaux à index_from
-     *
-     * @param index_from
-     */
-    /*private void modify_L(int index_from) {
-
-        ListIterator<Integer> it_L = L.subList(index_from, L.size()).listIterator();
-        int current_value;
-        while (it_L.hasNext()) {
-            current_value = it_L.next();
-            it_L.set(current_value + 1);
-        }
-    }
-*/
-	public void setL(int n , float Mat[][]) 
-	{ 
+		
 		int count; 
-		for(int i=0;i<n;i++) 
+		mL.add(0);
+		for(i=0;i<n;i++) 
 		{ 
 			count=0; 
-			for(int j=0;j<n;j++) 
+			for(j=0;j<n;j++) 
 			{ 
-				if(Mat[i][j]!=0) 
+				if(m.getMatrice()[i][j]!=0) 
 				{
 					count+=1;
 				}
@@ -140,9 +56,135 @@ public class CLI
 				mL.add(count+mL.get(mL.size()-1)); 
 			}
 		}
+		
 	}
 
-	public void setI(int n, float Mat[][]) {
+	public CLI (Float[] c, Integer[] l, Integer[] i) 
+	{
+		mC = new LinkedList<Float>(Arrays.asList(c));
+		mL = new LinkedList<Integer>(Arrays.asList(l));
+		mI = new LinkedList<Integer>(Arrays.asList(i));
+		this.n = l.length - 1;
+	}
+	
+	public  CLI(int size)
+	{
+		n = size;
+		mC = new LinkedList<Float>();
+		mL = new LinkedList<Integer>();
+		mI = new LinkedList<Integer>();
+		for (int i = 0; i <= this.n; i++) 
+		{
+			this.mL.add(0);
+		}
+	}
+
+	/**
+	 * @return the mC
+	 */
+	public LinkedList<Float> getmC() {
+		return mC;
+	}
+
+	/**
+	 * @param mC the mC to set
+	 */
+	public void setmC(LinkedList<Float> mC) 
+	{
+		this.mC = mC;
+	}
+
+	/**
+	 * @return the mL
+	 */
+	public LinkedList<Integer> getmL() {
+		return mL;
+	}
+	 /**
+	  * Incrémente les valeurs de mL d'index supérieurs ou égaux à index_from
+	  *
+	  * @param index_from
+	  */
+
+	 private void setmL(int index_from) 
+	 {
+		 ListIterator<Integer> it_L = mL.subList(index_from, mL.size()).listIterator();
+		 int current_value;
+		 while (it_L.hasNext()) {
+			 current_value = it_L.next();
+			 it_L.set(current_value + 1);
+		 }
+	 }
+	/**
+	 * @return the mI
+	 */
+	public LinkedList<Integer> getmI() {
+		return mI;
+	}
+
+	/**
+	 * @param mI the mI to set
+	 */
+	public void setmI(LinkedList<Integer> mI) {
+		this.mI = mI;
+	}
+
+	 /**
+	  * A enlever car j'utilise une matrice pour creer la matrice CLI
+	  * modifier une case de la matrice
+	  *
+	  * @param line
+	  * @param column
+	  * @param value
+	  */
+	
+	public void edit_value(int line, int column, float value) 
+	 {
+		 int line_start = mL.get(line), line_end = mL.get(line + 1), index = 0; //On récupère les positions de début et de fin d'une ligne à l'aide de L
+		 Iterator<Integer> it = mI.subList(line_start, mI.size()).iterator();
+		 boolean exist = false; //exist indique si la position de la case existe dans C
+
+		 if (line_end - line_start > 0) //Si la ligne a au moins une valeur non vide, on cherche la case à modifier
+		 { 
+			 int current;
+			 while (!exist && index < line_end - line_start) //tant que l'on a pas trouvé la case et que la ligne n'est pas entièrement parcourue
+			 { 
+				 current = it.next();
+
+				 if (current == column) 
+				 {
+					 this.mC.set(line_start + index, value); //Si l'on trouve la colone recherchée dans la section de I
+					 // correspondant à cette ligne, alors assigne value à la valeur correspondante dans C
+					 exist = true;
+
+				 } else if (current > column) //si on a atteint une colonne d'indice plus élevé sans la trouver, alors
+					 // on insère la valeur juste avant la case courrante
+				 {
+					 this.mC.add(line_start + index, value);
+					 this.mI.add(line_start + index, column);
+					 this.setmL(line + 1); ////on modifie L après l'insertion
+					 exist = true;
+				 }
+				 index++;
+			 }
+		 }
+		 if (!exist) //si la ligne est vide, on insère la valeur de la case.
+		 {
+			 this.mC.add(line_start + index, value);
+			 this.mI.add(line_start + index, column);
+			 this.setmL(line + 1); //on modifie L après l'insertion
+		 }
+	 }
+
+	/**
+	 * @return the n
+	 */
+	public int getN() {
+		return n;
+	}
+
+	public void setI(int n, float Mat[][]) 
+	{
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				if (Mat[i][j] != 0) {
@@ -151,32 +193,7 @@ public class CLI
 			}
 		}
 	}
-
-    @Override
-    public String toString() {
-        return "Matrice{" +
-                "C=" + mC +
-                ", L=" + mL +
-                ", I=" + mI +
-                ", n=" + n +
-                '}';
-    }
-    
-	void afficheCLI() {
-		for (int k = 0; k < mC.size(); k++) {
-			System.out.print(mC.get(k) + "\t");
-		}
-
-		System.out.print("\n");
-		for (int l = 0; l < mL.size(); l++) {
-			System.out.print(mL.get(l) + "\t");
-		}
-		System.out.print("\n");
-		for (int k = 0; k < mI.size(); k++) {
-			System.out.print(mI.get(k) + "\t");
-		}
-	}
-
+ 
 	public int maxI() {
 		int MAX = 0;
 		for (int k = 0; k < mI.size(); k++) {
@@ -185,128 +202,38 @@ public class CLI
 		}
 		return MAX;
 	}
-
-	/*
-	 * 
-	 * public float[][] getMatrice() {
-        float[][] mat = new float[n][n];
-
-        for (int i = 0, k = 1; i < I.size(); i++) {
-            if (i >= L.get(k)) {
-                do {
-                    k++;
-                } while (L.get(k).equals(L.get(k - 1)));
-            }
-            mat[k - 1][I.get(i)] = C.get(i);
-        }
-        return mat;
-    }
-
-    public void print_float_table(float[] vecteur) {
-        String str = "";
-        for (int i = 0; i < vecteur.length; i++) {
-            str += vecteur[i];
-            str += (i == vecteur.length - 1) ? "" : ", ";
-        }
-        System.out.println("[ " + str + "]");
-    }
-
-	public float[] mult_vect(float[] vecteur) {
-        float[] result = new float[n];
-        for (int i = 1; i < L.size(); i++) {
-            for (float j = L.get(i - 1); j < L.get(i); j++) {
-                result[i - 1] += C.get((int) j) * vecteur[I.get((int) j)];
-            }
-        }
-        return result;
-    }
-	 * 
-	 */
-	/*
-	public ArrayList<Float> matriceVector(ArrayList<Float> vect) {
-
-		ArrayList<Float> res = new ArrayList<Float>(mC.size());
-		float local = 0;
-		for (int i = 0; i < vect.size(); i++) {
-			res.set(i, (float) 0);
-
-			for (int j = mL.get(i); j <mL.get(i + 1); j++) {
-				local = local + mC.get(j) * vect.get(mI.get(j));
-				res.set(i, local);
-			}
-		}
-		return res;
-
-	}
-	*/
-	//produit de la matrice
-	public List<Float> produitMatrice(ArrayList<Float> vector) {
-
-		List<Float> res = new ArrayList<Float>();
-		int n = maxI() + 1;
-		
-		for (int i = 0; i < n; i++) 
-		{
-			res.add(i, 0.f);
-		}
-
-		for (int i = 0; i < n; i++) 
-		{
-			for (int j = mL.get(i); j < mL.get(i + 1); j++)
-			{
-				float index = res.get(mI.get(j)) + (mC.get(j) * vector.get(i));
-				res.set(mI.get(j), index);
-			}
-		}
-		return res;
-
-	}
 	
-/*
- * 
- * 
- *   public float[] mult_vect_transp(float[] vecteur) {
-        float[] result = new float[n];
+	 @Override
+	 public String toString() 
+	 {
+		 return "CLI {" +
+				 "C=" + mC +
+				 ", L=" + mL +
+				 ", I=" + mI +
+				 ", n=" + n +
+				 '}';
+	 }
+	
+	 public static void main(String[] args) 
+	 {
+		 Float[] C = {3f, 5f, 8f, 1f, 2f, 3f};
+		 Integer[] L = {0, 3, 5, 5, 6};
+		 Integer[] I = {1, 2, 3, 0, 2, 1};
+		 CLI c = new CLI(C, L, I);
+		 System.out.println("\nMatrice : " + c.toString());
 
-        for (int i = 1; i < L.size(); i++) 
-        {
-            for (float j = L.get(i - 1); j < L.get(i); j++) 
-            {
-                result[I.get((int) j)] += C.get((int) j) * vecteur[i - 1]; // On effectue les calculs non plus sur les
-                //colonnes de la matrice mais sur ses lignes
-            }
-        }
-        return result;
-    }
- * 
- */
-	//matrice transposée
-	public List<Float> vector_transpose(ArrayList<Float> vector) 
-	{
-		ArrayList<Float> res = new ArrayList<Float>(vector.size());
-		for (int i = 1; i < mL.size(); i++) 
-        {
-            for (float j = mL.get(i - 1); j < mL.get(i); j++) 
-            {
-            	float value = res.get(mI.get((int) j));
-            	value += mC.get((int) j) * vector.get(i - 1); // On effectue les calculs non plus sur les colonnes de la matrice mais sur ses lignes
-            	res.set(mI.get((int) j), value);
-            }
-        }
+		 //Matrice : CLI {C=[3.0, 5.0, 8.0, 1.0, 2.0, 3.0], L=[0, 3, 5, 5, 6], I=[1, 2, 3, 0, 2, 1], n=5}
+		
+		  Matrice m1 = new Matrice(4); // version alternative d'instancier une matrice
+	        m1.edit_value(0, 1, 3);
+	        m1.edit_value(0, 2, 5);
+	        m1.edit_value(0, 3, 8);
+	        m1.edit_value(1, 0, 1);
+	        m1.edit_value(1, 2, 2);
+	        m1.edit_value(3, 1, 3);
 
-		return res;
-	}
+			 System.out.println("\nMatrice : " + new CLI(m1).toString());
+			 //Matrice : CLI {C=[3.0, 5.0, 8.0, 1.0, 2.0, 3.0], L=[0, 3, 5, 5, 6], I=[1, 2, 3, 0, 2, 1], n=4}
 
-	// après avoir calculé le produit par la transposée, on modifie chaque valeur du vecteur afin de prendre en compte le facteur de zap
-	public List<Float> vector_transpose_zap(ArrayList<Float> vector, float zap) {
-		List<Float> result = this.vector_transpose(vector);
-
-        for (int i = 0; i < result.size(); i++) 
-        {
-        	float index = result.get(i) * (1 - zap) + (zap / this.n);// On effectue les calculs non plus sur les colonnes de la matrice mais sur ses lignes
-        	result.set(i, index);
-        }
-
-        return result;
-    }
+	 }
 }
